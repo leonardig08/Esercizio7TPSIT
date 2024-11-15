e45c9b8082680f0dfe5e2e067a9756e12fbe2c19
Giuseppe Leonardi <giuseppe.leonardi@itiscuneo.eu>
2024-11-15 10:35:41 +0100

    Finished Parallel Program

--- ClassLibrary7/Class1.cs
+++ ClassLibrary7/Class1.cs
@@ -22,7 +22,96 @@
         }
         public void MetodoParallelo()
         {
-            int A, B, C, D, E, F, G, H;
+            int A = 0, B = 0, C = 0, D = 0, E = 0, F = 0, G = 0, H = 0;
+
+            Thread assegnaA = new Thread(() =>
+            {
+                A = 10;
+            });
+            assegnaA.Start();
+            Thread assegnaB = new Thread(() =>
+            {
+                B = 20;
+            });
+            assegnaB.Start();
+            Thread assegnaC = new Thread(() =>
+            {
+                C = 30;
+            });
+            assegnaC.Start();
+
+            assegnaA.Join();
+            assegnaB.Join();
+            assegnaC.Join();
+
+            Thread calcolaD = new Thread(() =>
+            {
+                D = A + 5;
+            });
+            calcolaD.Start();
+            Thread CalcolaE = new Thread(() =>
+            {
+                E = B * 2;
+            });
+            CalcolaE.Start();
+            Thread CalcolaF = new Thread(() =>
+            {
+                F = C * C;
+            });
+            CalcolaF.Start();
+
+            calcolaD.Join();
+            CalcolaE.Join();
+            CalcolaF.Join();
+
+            Thread ScriviD = new Thread(() =>
+            {
+                Console.WriteLine("D: " + D);
+            });
+            ScriviD.Start();
+            Thread ScriviE = new Thread(() =>
+            {
+                Console.WriteLine("E: " + E);
+            });
+            ScriviE.Start();
+            Thread ScriviF = new Thread(() =>
+            {
+                Console.WriteLine("F: " + F);
+            });
+            ScriviF.Start();
+
+            ScriviD.Join();
+            ScriviE.Join();
+            ScriviF.Join();
+
+            Thread CalcolaG = new Thread(() =>
+            {
+                G = E - D;
+            });
+            CalcolaG.Start();
+
+            Thread CalcolaH = new Thread(() =>
+            {
+                H = F + G;
+            });
+            CalcolaH.Start();
+
+            CalcolaG.Join();
+            CalcolaH.Join();
+
+            Thread ScriviG = new Thread(() =>
+            {
+                Console.WriteLine("G: " + G);
+            });
+            ScriviG.Start();
+
+            Thread ScriviH = new Thread(() =>
+            {
+                Console.WriteLine("H: " + H);
+            });
+            ScriviH.Start();
+            ScriviG.Join();
+            ScriviH.Join();
         }
     }
 }
--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -1,4 +1,8 @@
 ﻿using ClassLibrary7;
 
 ClassLibrary7.Work work = new();
+Console.WriteLine("METODO SEQUENZIALE -------------------");
 work.MetodoSequenziale();
+Console.WriteLine("METODO PARALLELO ----------------------");
+work.MetodoParallelo();
+
